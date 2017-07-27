7995fdf6dcb517c3c4f5967138fcf922d79c5f98
Deomid Ryabkov <rojer@cesanta.com>
2017-07-27 13:09:33 +0200

    Add automated check for extern "C"
    Fix headers that didn't have it
    
    PUBLISHED_FROM=ce8140783d4b661f16278a4a5adc957b21965473

--- src/mgos_vfs_dev_spi_flash.h
+++ src/mgos_vfs_dev_spi_flash.h
@@ -10,6 +10,14 @@
 
 #define MGOS_VFS_DEV_TYPE_SPI_FLASH "spi_flash"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 bool mgos_vfs_dev_spi_flash_init(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* CS_MOS_LIBS_VFS_DEV_SPI_FLASH_SRC_MGOS_VFS_DEV_SPI_FLASH_H_ */
